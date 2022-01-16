c350c9f021b5122c46f809dec0159998f8ca3626
brian-lets-go <brian.w.meenan@gmail.com>
2022-01-16 17:45:20 -0500

    added javascript

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
-
 var questionIndex = 0;
 var time = 75;
+var timer
 
 var timeEl = document.querySelector("#time");
 var startBtn = document.querySelector("#start-button");
@@ -15,3 +15,39 @@ var highScorePageEl = document.querySelector("#high-score-display");
 
 var questionsEl = document.querySelector("#question");
 var choicesEl = document.querySelector("#choices");
+
+
+
+//function to begin quiz
+//start timer, switch screens, call pullQuestion()
+function quizStart() {
+    
+    startSection.setAttribute("class", "hide");
+
+    quizSection.setAttribute("class", "show");
+
+    timer = setInterval(second, 1000);
+
+    timeEl.textContent = time;
+
+    pullQuestion();
+
+}
+
+//set function for setInterval
+function second() {
+    
+    if (time <= 0) {
+        
+        endQuiz();
+    
+    } else {
+        
+        time--;
+        timeEl.textContent = time;
+
+    }
+}
+
+
+startBtn.onclick = quizStart();
