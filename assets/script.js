f2cddff5b821fa730866608e79ee1e2db473eb8a
brian-lets-go <brian.w.meenan@gmail.com>
2022-01-16 17:02:25 -0500

    finished highscores.html

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,17 @@
 
+var questionIndex = 0;
+var time = 75;
 
 var timeEl = document.querySelector("#time");
 var startBtn = document.querySelector("#start-button");
+var submitBtn = document.querySelector("#submit-button");
+var initialsEl = document.querySelector("#initials");
+var responseEl = document.querySelector("#response");
 
 var startSection = document.querySelector("#start-section");
-var titleSection = document.querySelector("#title-section");
 var quizSection = document.querySelector("#quiz-section");
 var highScoreScreen = document.querySelector("#high-score-section");
+var highScorePageEl = document.querySelector("#high-score-display");
 
+var questionsEl = document.querySelector("#question");
+var choicesEl = document.querySelector("#choices");
