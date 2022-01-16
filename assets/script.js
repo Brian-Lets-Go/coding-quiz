124fda79726edec01123e517ade3939dc46c702d
brian-lets-go <brian.w.meenan@gmail.com>
2022-01-16 16:25:59 -0500

    finish index.html

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+
+
+var timeEl = document.querySelector("#time");
+var startBtn = document.querySelector("#start-button");
+
+var startSection = document.querySelector("#start-section");
+var titleSection = document.querySelector("#title-section");
+var quizSection = document.querySelector("#quiz-section");
+var highScoreScreen = document.querySelector("#high-score-section");
+
